ac79171d0fd1063658fb9c5a6c4cb9f1de36db9d
Chun-Wei-Chu <max2916@gmail.com>
2017-01-04 10:03:25 +0800

    動態顯示table

---
+++ Table/example.js
@@ -0,0 +1,262 @@
+buildTable(tabledata, "body");
+
+function buildTable(tabledata, target)
+{
+	this.target = target;
+	setTimeout(
+      function(){
+        var DivClassByFreq = $("<div></div>").appendTo($(target));
+
+        setTimeout(
+          function(){
+            DivClassByFreq.html(
+              '<div><tr><div style="height:20px;"></div></tr><tr><div style="color:red;"> <div style="display:inline;float:right;color:black;"><input type="text" size="10"  onkeyup="ToDoScrollToSearch(event, this);"><button onclick="scrollToSearch(this.parentNode.childNodes[0].value, this.parentNode.parentNode.parentNode);">搜尋</button></div></div></tr></div>'
+            );	
+
+            //freq table's header
+            setTimeout(
+              function(){
+                var tdLenArr = [];
+                var tdLenArr_total_length=20;
+                DivClassByFreq = $("<div></div>").appendTo(DivClassByFreq)
+                  .attr("class", "buildReplyTable_innerTable");
+
+                var tmpTarget = $("<tr></tr>").appendTo(
+					$("<div></div>").appendTo(DivClassByFreq)
+					  .attr("class", "thead")
+					  .attr("style", "display:inline;width:100%;position:relative;")
+				).attr("style", "background:#dee;").attr("stick", 3);
+                var tmpTr=[];
+                for(var j=0; j < tabledata.thead.length ; j++)
+                {
+                  var tmpText = tabledata.thead[j];
+
+                  tmpTr.push('<td class="columnName" style="border:solid 1px #abc;text-align:center;word-break: keep-all;" colkey="'+j+'"><div style="width:'+(tmpText.length*10+20)+'px">'+tmpText+'</div></td>');
+                  tdLenArr.push(tmpText.length*10+20);
+                  tdLenArr_total_length+=tmpText.length*10+20;
+                }
+
+                DivClassByFreq.attr("style", "table-layout: fixed;overflow:hidden;width:"+ Math.min(($(this.target).width()-70),tdLenArr_total_length+5)+"px");
+
+                //加入resize事件，僅調整內頁用於置放table的div的寬度`, 以className為buildReplyTable_innerTable搜尋
+                window.addEventListener("resize", function() {
+                  DivClassByFreq.width(Math.min(DivClassByFreq.parent().width()-70, tdLenArr_total_length+5));
+                });
+
+                setTimeout(
+                  function(){
+                    tmpTarget.html(tmpTr.join(''));
+
+                    /*動態調整table邊界 以避免高度過高問題*/
+                    var tbodyheight=46*tabledata.tbody.length;
+                    tbodyheight = Math.min(tbodyheight, ($(this.target).height()/2));
+
+                    setTimeout(buildTable_tbody(tabledata.tbody, $("<div></div>").appendTo(DivClassByFreq).attr("class", "tbody").attr("style", "width:100%;text-align:right;display:block;overflow-y:scroll;height:"+tbodyheight+"px;"), tmpTarget, tdLenArr), 0);
+                  }.call(this),0);
+
+              }.call(this) ,0);
+
+          }.call(this) ,0);
+
+      }.call(this),0);
+}
+
+/*一列一列畫*/
+//stick 相關是用來做效果的
+function buildTable_tbody(data, target, header, tdLenArr)
+{
+  var tmpThis = this,
+    body = target,  //記錄下來以免多次查詢，用一點記憶體換時間
+    arrayTable=[];  //儲存所有已建好的資料<tr><td>data</td><tr>
+
+  target = $("<div></div>").appendTo($("<div></div>").appendTo(target).attr("style", "height:"+((data.length+1)*40)+"px")).attr("style", "position:relative").attr("class", "buildReplyTable_tbody_scollerY");
+
+
+  /*建立scroller事件*/
+  buildReplyTable_scrollerSet(body, header.parent(), target, arrayTable);
+
+  var j = 0,
+   Loop = data.length,
+   requestAnimationFrame =
+   window.requestAnimationFrame ||
+   window.mozRequestAnimationFrame ||
+   window.webkitRequestAnimationFrame ||
+   window.msRequestAnimationFrame,
+
+   /*透過遞迴呼叫防止畫面定格*/
+   doRequestFrame = function() {
+     if (j < Loop) {
+       /*js 對於純數值運算(不用顯示結果的那種)非常的快，但如果資料過大可能會花很長時間而失去回應性，在這以100組為單位平衡回應性*/
+       for(var l=0;l<100&&j<Loop;l++, j++){
+        var tmpTr = [];
+        for (var k = 0; k < data[j].length; k++) {
+          var s = (data[j][k] == null ? "null" : data[j][k]);
+          var output = (s!="null"?Math.round(parseFloat(("" + s))*100)/100:(`<span style="color:red">${s}<span>`));
+          tmpTr.push('<td style="border:solid 1px #abc;height:40px;value:2;"><div style="width:' + tdLenArr[k] + 'px">' + output + '</div></td>');
+         }
+
+         /*如果已經終止則不再做任何動作*/
+         if(tmpThis.Timeout==1)
+         {
+           return;
+         }
+
+         //$(target[0]).append('<tr stick="0">' + tmpTr.join('') + '</tr>');
+         arrayTable.push('<tr stick="0" onmouseover="buildReplyTable_onmouseover(this)" onmouseout="buildReplyTable_onmouseout(this)" onclick="buildReplyTable_onclick(this)">' + tmpTr.join('') + '</tr>');
+       }
+
+       if (requestAnimationFrame) {
+         requestAnimationFrame(doRequestFrame);
+       } else {
+         setTimeout(doRequestFrame, 0);
+       }
+     }
+     else
+     {
+       /*初始化內容*/
+       target.parent().parent().parent().parent()[0].arrayTable=arrayTable;
+       if(target.html()=="")
+       {
+         var tmpHTML = "";
+         for(var i=0 ; i < 40 && i < arrayTable.length;i++)
+         {
+           tmpHTML+=arrayTable[i];
+         }
+         target.html(tmpHTML);
+       }
+     }
+   }
+  requestAnimationFrame ? requestAnimationFrame(doRequestFrame) : doRequestFrame();
+}
+
+function buildReplyTable_onmouseover(tmp)
+{
+  if(tmp.getAttribute('stick')==0)tmp.style.background='#cde';
+}
+function buildReplyTable_onmouseout(tmp)
+{
+  if(tmp.getAttribute('stick')==0)tmp.style.background='';
+}
+function buildReplyTable_onclick(tmp)
+{
+  tmp.setAttribute('stick', tmp.getAttribute('stick')^1);
+}
+
+/*配置scroller，將全螢幕模式和一般模式的scroller統一管理*/
+/*("scroller所在的element(會顯示的畫面)", "table header", "最內層可移動的空間", "資料table格式為[<tr><td>...</td>...</tr>, ..., <tr><td>...</td>...</tr>]的array")*/
+function buildReplyTable_scrollerSet(tbody, thead, ScrollTr, arrayTable)
+{
+  var last_scollerY = 0,
+    last_scollerX = 0;
+
+
+  tbody.scroll(function(e) {
+    if(last_scollerY!=this.scrollTop)
+    {
+      if(!this.last_start)this.last_start = 0;
+      var tmpArray = ScrollTr.html().split("</tr>");
+      var tmpArray = ScrollTr.html().split("</tr>");
+      for(var i = this.last_start, j = 0; i < this.last_start+20 && i < arrayTable.length; i++, j++)
+      {	    
+        if(tmpArray[j].indexOf('stick="1"')!=-1 || (arrayTable[i].indexOf('stick="1"')!=-1 && tmpArray[j].indexOf('stick="0"')!=-1))
+          arrayTable[i] = tmpArray[j] + "</tr>";
+      }
+
+      ScrollTr.css({
+        top: this.scrollTop + 'px'
+      });
+
+      var tmpHTML = "";
+      var start = Math.floor(this.scrollTop/40);
+
+      for(var i=start ; i < start+20 && i < arrayTable.length;i++)
+      {
+        tmpHTML+=arrayTable[i];
+      }
+      ScrollTr.html(tmpHTML);
+
+      /*重置*/
+      last_scollerY = this.scrollTop;
+      this.last_start = start;
+    }
+
+    if(last_scollerX!=this.scrollLeft)
+    {
+      thead.css({
+          left: -this.scrollLeft + 'px'
+      });
+
+      last_scollerX = this.scrollLeft;
+    }
+  });
+}
+
+/************************ search bar 會自動跳到指定td *************************/
+function ToDoScrollToSearch(event, tmpThis)
+{
+  if(event.keyCode == 13){
+       scrollToSearch(tmpThis.parentNode.childNodes[0].value, tmpThis.parentNode.parentNode.parentNode.parentNode)
+   }
+}
+
+function scrollToSearch(str, target)
+{
+  var divs = target.getElementsByClassName("columnName");
+
+  /*代表換搜尋主題了*/
+  if(target.str != str)
+  {
+    target.index = -1;
+    target.str = str;
+
+    /*全部換回黑色*/
+    if(str=="")
+    {
+      for (var i = 0; i < divs.length; i++)
+      {
+          $(divs[i].getElementsByTagName("div")).css("color","black");
+      }
+      return;
+    }
+
+    for (var i = 0; i < divs.length; i++) {
+      if(divs[i].getElementsByTagName("div")[0].innerText.toLowerCase().indexOf(str) != -1)
+      {
+          $(divs[i].getElementsByTagName("div")).css("color","Orange");
+      }
+      else
+      {
+        $(divs[i].getElementsByTagName("div")).css("color","black");
+      }
+    }
+  }
+
+  var lastSelect = target.index;
+  for (var i = target.index+1; i < divs.length; i++) {
+      var para = divs[i].getElementsByTagName("div");
+      var index = para[0].innerText.toLowerCase().indexOf(str);
+      if (index != -1) {
+        //para[0].scrollIntoView();
+        $(para[0]).css("color","red");
+        if(lastSelect > 0)
+        {
+          //找到新的就註銷舊的
+          $(divs[lastSelect].getElementsByTagName("div")).css("color","Orange");
+        }
+
+        $(target.childNodes[1].childNodes[1])[0].scrollLeft = $(para[0]).position().left;
+        target.index = i;
+        return;
+      }
+  }
+
+  //如果找不到就傳出alert，否則視為循環查詢
+  if(target.index < 0)
+    alert("找不到符合要求的欄位");
+  else
+  {
+    target.index = -1;
+    $(divs[lastSelect].getElementsByTagName("div")).css("color","Orange");
+    scrollToSearch(str, target);
+  }
+}
